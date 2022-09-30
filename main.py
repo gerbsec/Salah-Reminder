dff0d0b4b6e4642807725c4e9cf6a90d050dcaaf
gerbsec <albabawaseem@gmail.com>
2022-09-29 22:56:51 -0400

    pog?

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import discord
 from discord.ext import commands, tasks
 import datetime as dt
 from pray_times_calculator import PrayerTimesCalculator
+
+id_list = 568747537872322570, 708372232115847312, 965808322768941129
 bot = commands.Bot("$", intents=discord.Intents.default())
-token = ""
-channel_id = 965808322768941129
+token = "OTY1ODEwNjM2OTA5MDg0Njkz.Yl4nPw.6sYxPDjham83ezSN8OZerxsZgXw"
 calc = PrayerTimesCalculator(
     latitude=27.9506,
     longitude=-82.4572,
@@ -29,28 +30,34 @@ calc = PrayerTimesCalculator(
     isha_angle=15,
 )
 
-
-
 @bot.event
 async def on_ready():
     await salah.start()
 
 @tasks.loop(seconds=30)
 async def salah():
-    print('est')
-    message_channel = bot.get_channel(channel_id)
     times = calc.fetch_prayer_times()
     time = dt.datetime.now().time().strftime("%H:%M")
     if time == times['Fajr']:
-        await message_channel.send("It is Fajr time! Be sure to Pray Fajr!")
+        for channel_id in id_list:
+            message_channel = bot.get_channel(channel_id)
+            await message_channel.send("It is Fajr time! Be sure to Pray Fajr!")
     elif time == times['Dhuhr']:
-        await message_channel.send("It is Dhuhr time! Be sure to Pray Dhuhr!")
+        for channel_id in id_list:
+            message_channel = bot.get_channel(channel_id)
+            await message_channel.send("It is Dhuhr time! Be sure to Pray Dhuhr!")
     elif time == times['Asr']:
-        await message_channel.send("It is Asr time! Be sure to Pray Asr!")
+        for channel_id in id_list:
+            message_channel = bot.get_channel(channel_id)
+            await message_channel.send("It is Asr time! Be sure to Pray Asr!")
     elif time == times['Maghrib']:
-        await message_channel.send("It is Maghrib time! Be sure to Pray Maghrib!")
+        for channel_id in id_list:
+            message_channel = bot.get_channel(channel_id)
+            await message_channel.send("It is Maghrib time! Be sure to Pray Maghrib!")
     elif time == times['Isha']:
-        await message_channel.send("It is Ishaa time! Be sure to Pray Ishaa!")
+        for channel_id in id_list:
+            message_channel = bot.get_channel(channel_id)
+            await message_channel.send("It is Ishaa time! Be sure to Pray Ishaa!")
 
 if __name__ == "__main__":
     bot.run(token)
